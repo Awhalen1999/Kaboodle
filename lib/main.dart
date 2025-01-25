3ed5edee3e38f069f3369547bf402df37e77a82a
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2025-01-25 00:52:51 -0330

    Revert "flutter setup"
    This reverts commit 5bea994e56d7cd14d3b4efb62fd520ae5618667b.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
-import 'package:copackr/firebase_options.dart';
 import 'package:copackr/router.dart';
 import 'package:copackr/theme/dark_mode.dart';
 import 'package:copackr/theme/light_mode.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+void main() {
   runApp(const MyApp());
 }
 
