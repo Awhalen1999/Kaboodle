06a73bd61d2778e8e38ac589bb5458ed481e9353
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2025-06-11 22:08:27 -0230

    menu drawer

--- lib/shared/widgets/menu_drawer.dart
+++ lib/shared/widgets/menu_drawer.dart
@@ -73,7 +73,7 @@ class MenuDrawer extends StatelessWidget {
                     ),
                     const SizedBox(width: 4),
                     Text(
-                      "(5)",
+                      "(3)",
                       style: TextStyle(
                         fontSize: 12,
                         color: Theme.of(context).colorScheme.onSurfaceVariant,
@@ -98,128 +98,124 @@ class MenuDrawer extends StatelessWidget {
                   ],
                 ),
               ),
-              ListTile(
-                title: const Text('List 1'),
-                leading:
-                    const Icon(Icons.square_rounded, color: Colors.pinkAccent),
-                trailing: const CustomChip(),
-                onTap: () {
-                  Navigator.pop(context);
-                  context.push('/list-viewer');
-                },
-              ),
-              ListTile(
-                title: const Text('List 2'),
-                leading:
-                    const Icon(Icons.square_rounded, color: Colors.lightBlue),
-                trailing: const CustomChip(),
-                onTap: () {
-                  Navigator.pop(context);
-                  context.push('/list-viewer');
-                },
-              ),
-              ListTile(
-                title: const Text('List 3'),
-                leading: const Icon(Icons.square_rounded, color: Colors.amber),
-                trailing: const CustomChip(),
-                onTap: () {
-                  Navigator.pop(context);
-                  context.push('/list-viewer');
-                },
-              ),
-              const Divider(
-                color: Colors.grey,
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Row(
-                  children: [
-                    const Text(
-                      "Tags",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: const Text('List 1'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.pinkAccent),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
                       ),
-                    ),
-                    const SizedBox(width: 4),
-                    Text(
-                      "(9)",
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Theme.of(context).colorScheme.onSurfaceVariant,
-                        fontWeight: FontWeight.w400,
+                      ListTile(
+                        title: const Text('List 2'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.lightBlue),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
                       ),
-                    ),
-                    const Spacer(),
-                    IconButton(
-                      icon: const Icon(Icons.add_rounded),
-                      padding: EdgeInsets.all(
-                        0,
+                      ListTile(
+                        title: const Text('List 3'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.amber),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
                       ),
-                      constraints: const BoxConstraints(),
-                      style: const ButtonStyle(
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      ListTile(
+                        title: const Text('List 4'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.purple),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
                       ),
-                      onPressed: () {},
-                    ),
-                  ],
+                      ListTile(
+                        title: const Text('List 5'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.teal),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
+                      ),
+                      ListTile(
+                        title: const Text('List 6'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.teal),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
+                      ),
+                      ListTile(
+                        title: const Text('List 7'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.teal),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
+                      ),
+                      ListTile(
+                        title: const Text('List 8'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.teal),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
+                      ),
+                      ListTile(
+                        title: const Text('List 9'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.teal),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
+                      ),
+                      ListTile(
+                        title: const Text('List 10'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.teal),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
+                      ),
+                      ListTile(
+                        title: const Text('List 11'),
+                        leading: const Icon(Icons.square_rounded,
+                            color: Colors.teal),
+                        trailing: const CustomChip(),
+                        onTap: () {
+                          Navigator.pop(context);
+                          context.push('/list-viewer');
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
-                child: Wrap(
-                  spacing: 8.0,
-                  children: [
-                    Chip(
-                      label: const Text(
-                        'Work',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                      backgroundColor: Colors.lightBlue,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 4,
-                        vertical: 2,
-                      ),
-                    ),
-                    Chip(
-                      label: const Text(
-                        'Personal',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                      backgroundColor: Colors.green.shade100,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 4,
-                        vertical: 2,
-                      ),
-                    ),
-                    Chip(
-                      label: const Text(
-                        'Urgent',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                      backgroundColor: Colors.red.shade100,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 4,
-                        vertical: 2,
-                      ),
-                    ),
-                    Chip(
-                      label: const Text(
-                        'Travel',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                      backgroundColor: Colors.orange.shade100,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 4,
-                        vertical: 2,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const Spacer(),
               Container(
                 decoration: BoxDecoration(
                   border: Border(
